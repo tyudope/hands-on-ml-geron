f16bb2806af377f27af2c184789fd5ee488679a6
dopesthetic <150394396+tyudope@users.noreply.github.com>
2026-01-31 10:14:55 +0100

    Complete Chapter 2 end-to-end ML Project (pipelines, CV, hyperparameter tuning)

---
+++ part_01_fundamentals_of_machine_learning/chapter_02_end-to-end_machine_learning_project/import_housing_data.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import pandas as pd
+import tarfile
+import urllib.request
+
+
+def load_housing_data():
+
+    tarball_path = Path("datasets/housing.tgz")
+    if not tarball_path.is_file():
+        Path("datasets").mkdir(parents = True, exist_ok= True)
+        url = "https://github.com/ageron/data/raw/main/housing.tgz"
+        urllib.request.urlretrieve(url, tarball_path)
+        with tarfile.open(tarball_path) as housing_tarball:
+            housing_tarball.extractall(path = "datasets" , filter = "data")
+
+    return pd.read_csv(Path("datasets/housing/housing.csv"))
+
+
+housing_full = load_housing_data()
